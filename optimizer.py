1643bc1e25707efac7fd33e491cc746923e66b6d
LIvanoff <drfrily@gmail.com>
2023-04-18 00:00:17 +0300

    update optimizer.py

--- optimizer.py
+++ optimizer.py
@@ -1,4 +1,5 @@
 from torch.autograd import grad
+from network.layers import Linear
 import torch
 
 
@@ -8,11 +9,15 @@ class Optimizer(object):
 
 
 class SGD(Optimizer):
-    def __init__(self, lr, parameters: object):
+    def __init__(self, lr, model: object):
         super().__init__(lr)
 
     def step(self):
         with torch.no_grad():
+            for var in vars(object).values():
+                if isinstance(var, Linear):
+                    print(var.weight)
+
             # weight -= torch.mul(d_loss_dx_w, 0.01)
             # bias -= torch.mul(d_loss_dx_b, 0.01)
             pass
