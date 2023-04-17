645d30cd61c644941f93b57f27f5ea4a3a1bdf6a
LIvanoff <drfrily@gmail.com>
2023-04-18 01:35:13 +0300

    update optimizer.py, finished making the sgd optimizer

--- optimizer.py
+++ optimizer.py
@@ -4,24 +4,32 @@ import torch
 
 
 class Optimizer(object):
-    def __init__(self, lr: float = 0.001):
+    def __init__(self, model: object, lr: float = 1e-4):
         self.lr = lr
+        self.model = model
+
+    def step(self):
+        """
+        function performs a single optimization step (parameter update)
+        """
+        pass
+
+    def zero_grad(self):
+        with torch.no_grad():
+            for var in vars(self.model).items():
+                if isinstance(var[1], Linear):
+                    if var[1].weight.grad is not None:
+                        var[1].weight.grad = var[1].weight.grad.zero_()
+                        var[1].weight.bias = var[1].bias.grad.zero_()
 
 
 class SGD(Optimizer):
-    def __init__(self, lr, model: object):
-        super().__init__(lr)
+    def __init__(self, model: object, lr: float = 1e-4):
+        super().__init__(model, lr)
 
     def step(self):
         with torch.no_grad():
-            for var in vars(object).values():
-                if isinstance(var, Linear):
-                    print(var.weight)
-
-            # weight -= torch.mul(d_loss_dx_w, 0.01)
-            # bias -= torch.mul(d_loss_dx_b, 0.01)
-            pass
-
-    def zero_grad(self):
-        # .zero_()
-        pass
+            for var in vars(self.model).items():
+                if isinstance(var[1], Linear):
+                    var[1].weight -= torch.mul(var[1].weight.grad, self.lr)
+                    var[1].bias -= torch.mul(var[1].bias.grad, self.lr)
