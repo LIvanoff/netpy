640938cf2c8a7e4254996ed32addcf1000562593
LIvanoff <drfrily@gmail.com>
2023-04-17 19:10:02 +0300

    update loss_fn.py, changed numpy to torch

--- network/loss_fn.py
+++ network/loss_fn.py
@@ -1,23 +1,23 @@
-import numpy as np
+import torch
 
 
 def MSE(y, y_hat):
-    return np.mean(np.power((y - y_hat), 2))
+    return torch.mean(torch.pow((y - y_hat), 2))
 
 
 def RMSE(y, y_hat):
-    return np.sqrt(np.mean(np.power((y - y_hat), 2)))
+    return torch.sqrt(torch.mean(torch.pow((y - y_hat), 2)))
 
 
 def MAE(y, y_hat):
-    return np.mean(np.abs(y - y_hat))
+    return torch.mean(torch.abs(y - y_hat))
 
 
 def BCE(y, y_hat):
-    y_hat = np.clip(y_hat, 1e-10, 1 - 1e-10)
-    return np.mean(-(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat)))
+    y_hat = torch.clip(y_hat, 1e-10, 1 - 1e-10)
+    return torch.mean(-(y * torch.log(y_hat) + (1 - y) * torch.log(1 - y_hat)))
 
 
 def CrossEntropy(y, y_hat):
-    y_hat = np.clip(y_hat, 1e-10, 1 - 1e-10)
-    return -y * np.log(y_hat)
+    y_hat = torch.clip(y_hat, 1e-10, 1 - 1e-10)
+    return -y * torch.log(y_hat)
