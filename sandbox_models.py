d874f0a85db4cee424e8263d2bc1885873b88430
Leonid <drfrily@gmail.com>
2023-04-19 18:25:20 +0300

    update sandbox_models.py, new data

--- sandbox_models.py
+++ sandbox_models.py
@@ -80,147 +80,103 @@ class LogisticRegression(object):
     @staticmethod
     def plot(pred, pcolor):
         plt.clf()
-        plt.tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('Blues'), alpha=1)
-        plt.scatter(x[:, 0], x[:, 1], alpha=0.9, c=pred.detach().numpy())
+        plt.tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('coolwarm'), alpha=1)
+        plt.scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred.detach().numpy())
         plt.grid(alpha=0.2)
         plt.draw()
         plt.gcf().canvas.flush_events()
 
 
-x = torch.Tensor(
-    [[1., 1.1],
-     [1., 3.2],
-     [1.4, 2.1],
-     [2., 2.9],
-     [2.1, 4.2],
-     [2.5, 1.1],
-     [2.3, 2.1],
-     [2.9, 3.4],
-     [3.1, 2.6],
-     [3.4, 4.1],
-     [3.8, 1.5],
-     [3.9, 3.1],
-     [1.2, 1.3],
-     [1.1, 2.2],
-     [1.5, 2.1],
-     [2.1, 2.4],
-     [2.2, 4.1],
-     [2.1, 0.1],
-     [2., 2.1],
-     [2.7, 3.2],
-     [3.2, 2.7],
-     [3.3, 4.2],
-     [3.5, 1.5],
-     [3.7, 3.1],
+# x = torch.Tensor(
+#     [[1., 1.1],
+#      [1., 3.2],
+#      [1.4, 2.1],
+#      [2., 2.9],
+#      [2.1, 4.2],
+#      [2.5, 1.1],
+#      [2.3, 2.1],
+#      [2.9, 3.4],
+#      [3.1, 2.6],
+#      [3.4, 4.1],
+#      [3.8, 1.5],
+#      [3.9, 3.1],
+#
+#      [5.2, 5.],
+#      [5.6, 6.],
+#      [5.7, 7.],
+#      [5.5, 9.],
+#      [5.3, 8.2],
+#      [6.1, 6.2],
+#      [6.3, 7.2],
+#      [6.1, 8.5],
+#      [7., 7.1],
+#      [7.3, 6.8],
+#      [6.6, 7.3],
+#      [8.1, 6.2],
+#      [8.4, 6.7],
+#      [7.9, 8.2]])
+# y = torch.Tensor(
+#     [[0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [0],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1],
+#      [1]])
 
-     [5.2, 5.],
-     [5.6, 6.],
-     [5.7, 7.],
-     [5.5, 9.],
-     [5.3, 8.2],
-     [6.1, 6.2],
-     [6.3, 7.2],
-     [6.1, 8.5],
-     [7., 7.1],
-     [7.3, 6.8],
-     [6.6, 7.3],
-     [8.1, 6.2],
-     [8.4, 6.7],
-     [7.9, 8.2],
-     [5.1, 5.1],
-     [5.3, 6.],
-     [5.6, 7.2],
-     [5.5, 9.1],
-     [5., 8.1],
-     [6., 6.3],
-     [6.3, 7.1],
-     [6.2, 8.7],
-     [7.3, 7.6],
-     [7.2, 6.6],
-     [6.3, 7.8],
-     [8.2, 6.1],
-     [8.3, 6.1],
-     [7.2, 8.2]
-     ])
-y = torch.Tensor(
-    [[0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [0],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1],
-     [1]])
+x = torch.Tensor([[4.5, 5.], [4.3, 5.1], [4.5, 4.4], [5., 5.], [4.9, 4.8],[5.1, 5.], [5.3, 4.1], [5.2, 4.8], [5.1, 5.2], [4.9, 4.6],
+                  [4.3, 5.1], [4.2, 5.], [4.4, 4.2], [5.1, 5.3], [4.7, 4.8],[5., 5.], [5.2, 4.2], [5.1, 4.4], [5., 5.3], [4.7, 4.3],
+                  [6., 2.], [3., 4.5], [4., 3.],[5., 2.8], [5.5, 2.9], [6.8, 3.5], [7.1, 4.5], [6.5, 5.9],[6.3, 6.3], [4.5, 7.], [2.5, 6.],
+                  [6.1, 2.1], [6.1, 4.4], [3.5, 4.],[2.8, 3.], [2.9, 5.9], [3.5, 6.8], [4.5, 7.1], [5.9, 6.5],[6.2, 6.3], [4.6, 7.1], [2.3, 6.1]])
 
+y = torch.Tensor([[1.], [1.], [1.], [1.], [1.],[1.], [1.], [1.], [1.], [1.],
+                  [1.], [1.], [1.], [1.], [1.],[1.], [1.], [1.], [1.], [1.],
+                  [0.], [0.], [0.],[0.], [0.], [0.], [0.], [0.],[0.], [0.], [0.],
+                  [0.], [0.], [0.],[0.], [0.], [0.], [0.], [0.],[0.], [0.], [0.]])
 eps = 0.1
-xx, yy = np.meshgrid(np.linspace(0.62 - eps, 8.75 + eps, 30),
-                     np.linspace(-0.31 - eps, 9.37 + eps, 30))
+xx, yy = np.meshgrid(np.linspace(1.5 - eps, 7.5 + eps, 80),
+                     np.linspace(1.5 - eps, 8. + eps, 80))
 z = torch.Tensor(np.array([xx.ravel(), yy.ravel()]))
 z = z.T
 
-classificator = LogisticRegression(2, 9)
+classificator = LogisticRegression(2, 11)
 
-optim = optimizer.SGD(lr=1, model=classificator)
+optim = optimizer.SGD(lr=0.9, model=classificator)
 
 plt.ion()
-for epoch in range(1000):
+for epoch in range(20000):
     optim.zero_grad()
     pred = classificator.forward(x)
     loss = F.BCE(y, pred)
     loss.backward()
     optim.step()
-    if epoch % 10 == 0:
-        print(f'loss: {loss}')
+    if epoch % 100 == 0:
+        print(f'epoch: {epoch} loss: {loss}')
 
-    with torch.no_grad():
-        pcolor = classificator.forward(z)
-        pcolor = torch.reshape(pcolor, (-1,))
-
-    classificator.plot(pred, pcolor)
+    # with torch.no_grad():
+    #     pcolor = classificator.forward(z)
+    #     pcolor = torch.reshape(pcolor, (-1,))
+    #
+    # classificator.plot(pred, pcolor)
 
 plt.ioff()
 plt.show()
@@ -233,12 +189,13 @@ with torch.no_grad():
     pcolor_threshold = torch.reshape(pcolor_threshold, (-1,))
     pred = classificator.forward(x)
 
-fig, axs = plt.subplots(1, 3, figsize=(16, 5))
-axs[1].tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('Blues'), alpha=1)
-axs[2].tricontourf(xx.ravel(), yy.ravel(), pcolor_threshold, cmap=plt.get_cmap('Blues'), alpha=1)
+fig, axs = plt.subplots(1, 3, figsize=(12, 3.5))
+axs[1].tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('coolwarm'), alpha=1)
+axs[2].tricontourf(xx.ravel(), yy.ravel(), pcolor_threshold, cmap=plt.get_cmap('coolwarm'), alpha=1)
 axs[0].scatter(x[:, 0], x[:, 1], alpha=0.8, c=y)
 axs[1].scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred)
 axs[2].scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred)
 axs[0].grid(alpha=0.2)
 axs[1].grid(alpha=0.2)
+axs[2].grid(alpha=0.2)
 plt.show()
