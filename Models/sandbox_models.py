d32d210f1a5233f02dc07e7c6c4b2e1a779cce3d
LIvanoff <drfrily@gmail.com>
2023-04-20 02:57:51 +0300

    update sandbox_models.py

--- Models/sandbox_models.py
+++ Models/sandbox_models.py
@@ -10,7 +10,7 @@ import time
 class Regression(object):
     def __init__(self, n_input, n_hidden):
         self.fc1 = nn.Linear(n_input, n_hidden)
-        self.activ1 = nn.LeakyReLU()
+        self.activ1 = nn.ReLU()
 
     def forward(self, x):
         out = self.fc1(x)
@@ -25,7 +25,6 @@ class Regression(object):
         plt.grid(alpha=0.2)
         plt.draw()
         plt.gcf().canvas.flush_events()
-        time.sleep(0.0001)
 
 
 x = torch.Tensor([[1.], [1.2], [1.6], [1.78], [2], [2.3], [2.4], [3], [3.3], [4.], [4.1], [4.12], [4.34], [5], [5.3], [5.6], [6]])
@@ -60,7 +59,8 @@ with torch.no_grad():
 plt.scatter(x, y, marker='o', alpha=0.8)
 plt.plot(x, pred.detach().numpy(), 'r')
 plt.grid(alpha=0.2)
-plt.show()"""
+plt.show()
+"""
 
 
 class LogisticRegression(object):
@@ -78,10 +78,19 @@ class LogisticRegression(object):
         return out
 
     @staticmethod
-    def plot(pred, pcolor):
+    def plot_figure(pred, pcolor):
         plt.clf()
         plt.tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('coolwarm'), alpha=1)
         plt.scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred.detach().numpy())
+        plt.grid(alpha=0.2)
+        plt.draw()
+        plt.gcf().canvas.flush_events()
+
+    @staticmethod
+    def plot_loss(x_loss, loss_history):
+        plt.clf()
+        plt.plot(x_loss, loss_history)
+        plt.show()
         plt.grid(alpha=0.2)
         plt.draw()
         plt.gcf().canvas.flush_events()
@@ -143,10 +152,13 @@ class LogisticRegression(object):
 #      [1],
 #      [1]])
 
-x = torch.Tensor([[4.5, 5.], [4.3, 5.1], [4.5, 4.4], [5., 5.], [4.9, 4.8],[5.1, 5.], [5.3, 4.1], [5.2, 4.8], [5.1, 5.2], [4.9, 4.6],
-                  [4.3, 5.1], [4.2, 5.], [4.4, 4.2], [5.1, 5.3], [4.7, 4.8],[5., 5.], [5.2, 4.2], [5.1, 4.4], [5., 5.3], [4.7, 4.3],
-                  [6., 2.], [3., 4.5], [4., 3.],[5., 2.8], [5.5, 2.9], [6.8, 3.5], [7.1, 4.5], [6.5, 5.9],[6.3, 6.3], [4.5, 7.], [2.5, 6.],
-                  [6.1, 2.1], [6.1, 4.4], [3.5, 4.],[2.8, 3.], [2.9, 5.9], [3.5, 6.8], [4.5, 7.1], [5.9, 6.5],[6.2, 6.3], [4.6, 7.1], [2.3, 6.1]])
+x = torch.Tensor(
+    [[4.5, 5.], [4.3, 5.1], [4.5, 4.4], [5., 5.], [4.9, 4.8], [5.1, 5.], [5.3, 4.1], [5.2, 4.8], [5.1, 5.2], [4.9, 4.6],
+     [4.3, 5.1], [4.2, 5.], [4.4, 4.2], [5.1, 5.3], [4.7, 4.8], [5., 5.], [5.2, 4.2], [5.1, 4.4], [5., 5.3], [4.7, 4.3],
+     [6., 2.], [3., 4.5], [4., 3.], [5., 2.8], [5.5, 2.9], [6.8, 3.5], [7.1, 4.5], [6.5, 5.9], [6.3, 6.3], [4.5, 7.],
+     [2.5, 6.],
+     [6.1, 2.1], [6.1, 4.4], [3.5, 4.], [2.8, 3.], [2.9, 5.9], [3.5, 6.8], [4.5, 7.1], [5.9, 6.5], [6.2, 6.3],
+     [4.6, 7.1], [2.3, 6.1]])
 
 y = torch.Tensor([[1.], [1.], [1.], [1.], [1.],[1.], [1.], [1.], [1.], [1.],
                   [1.], [1.], [1.], [1.], [1.],[1.], [1.], [1.], [1.], [1.],
@@ -158,12 +170,15 @@ xx, yy = np.meshgrid(np.linspace(1.5 - eps, 7.5 + eps, 80),
 z = torch.Tensor(np.array([xx.ravel(), yy.ravel()]))
 z = z.T
 
-classificator = LogisticRegression(2, 11)
+classificator = LogisticRegression(2, 15)
 
-optim = optimizer.SGD(lr=0.9, model=classificator)
+optim = optimizer.SGD(lr=0.4, model=classificator)
+
+loss_history = np.array([])
+x_loss = np.array([])
 
 plt.ion()
-for epoch in range(20000):
+for epoch in range(7000):
     optim.zero_grad()
     pred = classificator.forward(x)
     loss = F.BCE(y, pred)
@@ -171,12 +186,14 @@ for epoch in range(20000):
     optim.step()
     if epoch % 100 == 0:
         print(f'epoch: {epoch} loss: {loss}')
-
-    # with torch.no_grad():
-    #     pcolor = classificator.forward(z)
-    #     pcolor = torch.reshape(pcolor, (-1,))
-    #
-    # classificator.plot(pred, pcolor)
+        loss_history = np.append(loss_history, loss.detach().numpy())
+        x_loss = np.append(x_loss, epoch)
+        # with torch.no_grad():
+        #     pcolor = classificator.forward(z)
+        #     pcolor = torch.reshape(pcolor, (-1,))
+        #
+        # classificator.plot(pred, pcolor)
+        classificator.plot_loss(x_loss, loss_history)
 
 plt.ioff()
 plt.show()
@@ -189,13 +206,15 @@ with torch.no_grad():
     pcolor_threshold = torch.reshape(pcolor_threshold, (-1,))
     pred = classificator.forward(x)
 
-fig, axs = plt.subplots(1, 3, figsize=(12, 3.5))
+fig, axs = plt.subplots(1, 4, figsize=(12, 3))
 axs[1].tricontourf(xx.ravel(), yy.ravel(), pcolor, cmap=plt.get_cmap('coolwarm'), alpha=1)
 axs[2].tricontourf(xx.ravel(), yy.ravel(), pcolor_threshold, cmap=plt.get_cmap('coolwarm'), alpha=1)
 axs[0].scatter(x[:, 0], x[:, 1], alpha=0.8, c=y)
 axs[1].scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred)
 axs[2].scatter(x[:, 0], x[:, 1], alpha=0.8, c=pred)
+axs[3].plot(x_loss, loss_history)
 axs[0].grid(alpha=0.2)
 axs[1].grid(alpha=0.2)
 axs[2].grid(alpha=0.2)
+axs[3].grid(alpha=0.2)
 plt.show()
