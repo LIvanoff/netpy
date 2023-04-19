db0caf13de6ed4a7a55183cdca6dcfa694920974
Leonid <drfrily@gmail.com>
2023-04-19 18:24:45 +0300

    update optimizer.py, add StepScheduler

--- optimizer.py
+++ optimizer.py
@@ -31,3 +31,26 @@ class SGD(Optimizer):
                 if isinstance(var[1], Linear):
                     var[1].weight -= torch.mul(var[1].weight.grad, self.lr)
                     var[1].bias -= torch.mul(var[1].bias.grad, self.lr)
+
+
+class StepScheduler(object):
+    lr: float
+
+    def __init__(self, optimizer: object):
+        self.optimizer = optimizer
+        for var in vars(self.optimizer).items():
+            if isinstance(var[1], float):
+                self.lr = var[1].lr
+
+
+class StepLR(StepScheduler):
+    step_size: int
+    gamma: float
+
+    def __init__(self, optimizer: object, step_size: int, gamma: float):
+        super().__init__(optimizer)
+        self.step_size = step_size
+        self.gamma = gamma
+
+    def step(self):
+        pass
