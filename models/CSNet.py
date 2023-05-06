f87efdb959c7496cf380ef8a1dad4cc0c14fa1a1
Leonid <drfrily@gmail.com>
2023-05-07 01:01:39 +0300

    update CSNet.py

--- models/CSNet.py
+++ models/CSNet.py
@@ -1,6 +1,7 @@
 import network.layers as nn
 import network.loss_fn as F
 import optimizer
+import pandas as pd
 import matplotlib.pyplot as plt
 import torch.autograd
 import numpy as np
@@ -10,22 +11,36 @@ import time
 class CSNet(object):
     def __init__(self, n_input, n_hidden):
         self.fc1 = nn.Linear(n_input, n_hidden)
-
         self.activ1 = nn.ReLU()
-        self.fc2 = nn.Linear(n_input, 9)
+        self.fc2 = nn.Linear(n_hidden, 7)
         self.activ2 = nn.ReLU()
-        self.fc3 = nn.Linear(9, 8)
+        self.fc3 = nn.Linear(7, 6)
         self.activ3 = nn.ReLU()
-        self.fc4 = nn.Linear(8, 7)
+        self.fc4 = nn.Linear(6, 5)
         self.activ4 = nn.ReLU()
-        self.fc5 = nn.Linear(7, 8)
+        self.fc5 = nn.Linear(5, 6)
         self.activ5 = nn.ReLU()
-        self.fc6 = nn.Linear(8, 9)
+        self.fc6 = nn.Linear(6, 7)
         self.activ6 = nn.ReLU()
-        self.fc7 = nn.Linear(9, 10)
+        self.fc7 = nn.Linear(7, 8)
         self.activ7 = nn.ReLU()
-        self.fc8 = nn.Linear(10, 2)
+        self.fc8 = nn.Linear(8, 2)
         self.activ8 = nn.Softmax()
+
+        # self.activ1 = torch.nn.ReLU()
+        # self.fc2 = torch.nn.Linear(n_input, 9)
+        # self.activ2 = torch.nn.ReLU()
+        # self.fc3 = torch.nn.Linear(9, 8)
+        # self.activ3 = torch.nn.ReLU()
+        # self.fc4 = torch.nn.Linear(8, 7)
+        # self.activ4 = torch.nn.ReLU()
+        # self.fc5 = torch.nn.Linear(7, 8)
+        # self.activ5 = torch.nn.ReLU()
+        # self.fc6 = torch.nn.Linear(8, 9)
+        # self.activ6 = torch.nn.ReLU()
+        # self.fc7 = torch.nn.Linear(9, 10)
+        # self.activ7 = torch.nn.ReLU()
+        # self.fc8 = torch.nn.Linear(10, 2)
 
         # self.activ1 = nn.Sigmoid()
         # self.fc2 = nn.Linear(n_input, 9)
@@ -62,28 +77,37 @@ class CSNet(object):
         return out
 
 
-x = torch.empty(size=(10, 10)).normal_()
-y = torch.eye(10, 2)
+# x = torch.empty(size=(20000, 10)).normal_()
+# y = torch.eye(20000, 2)
 
-csnet = CSNet(10, 10)
-optim = optimizer.SGD(lr=0.4, model=csnet)
+train = pd.read_excel('csnet.xlsx', engine='openpyxl')
+data = train.values
+y = torch.LongTensor(data[:, 8:10])
+print(y)
+# y = torch.transpose(y, 0, 1)
+# y = torch.LongTensor(y[0,:])
+x = torch.Tensor(data[:, :8])
+print(x)
+
+csnet = CSNet(8, 8)
+optim = optimizer.SGD(lr=0.03, model=csnet)
 
 loss_history = np.array([])
 x_loss = np.array([])
 
 start_time = time.time()
-for epoch in range(1):
+for epoch in range(10000):
     optim.zero_grad()
     pred = csnet.forward(x)
-    print(pred)
     loss = F.CrossEntropy(y, pred)
     loss.backward()
     optim.step()
 
-    if epoch % 10 == 0:
+    if epoch % 100 == 0:
         print(f'epoch: {epoch} loss: {loss}')
-        loss_history = np.append(loss_history, loss.detach().numpy())
-        x_loss = np.append(x_loss, epoch)
+
+    loss_history = np.append(loss_history, loss .detach().numpy())
+    x_loss = np.append(x_loss, epoch)
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
