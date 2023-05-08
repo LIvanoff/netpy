d7e67dd477e978fd950b30e48b16216254054091
LIvanoff <drfrily@gmail.com>
2023-05-09 00:09:00 +0300

    update CSNet.py

--- models/csnet/CSNet.py
+++ models/csnet/CSNet.py
@@ -12,19 +12,34 @@ class CSNet(object):
     def __init__(self, n_input, n_hidden):
         self.fc1 = nn.Linear(n_input, n_hidden)
         self.activ1 = nn.ReLU()
-        self.fc2 = nn.Linear(n_hidden, 7)
+        # self.fc2 = nn.Linear(n_hidden, 9)
+        # self.activ2 = nn.ReLU()
+        # self.fc3 = nn.Linear(9, 8)
+        # self.activ3 = nn.ReLU()
+        # self.fc4 = nn.Linear(8, 7)
+        # self.activ4 = nn.ReLU()
+        # self.fc5 = nn.Linear(7, 8)
+        # self.activ5 = nn.ReLU()
+        # self.fc6 = nn.Linear(8, 9)
+        # self.activ6 = nn.ReLU()
+        # self.fc7 = nn.Linear(9, 10)
+        # self.activ7 = nn.ReLU()
+        # self.fc8 = nn.Linear(10, 2)
+        # self.activ8 = nn.Softmax()
+
+        self.fc2 = nn.Linear(n_hidden, 9)
         self.activ2 = nn.ReLU()
-        self.fc3 = nn.Linear(7, 6)
+        self.fc3 = nn.Linear(9, 8)
         self.activ3 = nn.ReLU()
-        self.fc4 = nn.Linear(6, 5)
+        self.fc4 = nn.Linear(8, 7)
         self.activ4 = nn.ReLU()
-        self.fc5 = nn.Linear(5, 6)
+        self.fc5 = nn.Linear(7, 8)
         self.activ5 = nn.ReLU()
-        self.fc6 = nn.Linear(6, 7)
+        self.fc6 = nn.Linear(8, 9)
         self.activ6 = nn.ReLU()
-        self.fc7 = nn.Linear(7, 8)
+        self.fc7 = nn.Linear(9, 10)
         self.activ7 = nn.ReLU()
-        self.fc8 = nn.Linear(8, 2)
+        self.fc8 = nn.Linear(10, 2)
         self.activ8 = nn.Softmax()
 
         # self.activ1 = torch.nn.ReLU()
@@ -43,7 +58,7 @@ class CSNet(object):
         # self.fc8 = torch.nn.Linear(10, 2)
 
         # self.activ1 = nn.Sigmoid()
-        # self.fc2 = nn.Linear(n_input, 9)
+        # self.fc2 = nn.Linear(n_hidden, 9)
         # self.activ2 = nn.Sigmoid()
         # self.fc3 = nn.Linear(9, 8)
         # self.activ3 = nn.Sigmoid()
@@ -77,46 +92,64 @@ class CSNet(object):
         return out
 
 
-train = pd.read_excel('csnet.xlsx', engine='openpyxl')
+train = pd.read_excel('output.xlsx', engine='openpyxl')
 data = train.values
-y = torch.LongTensor(data[:, 8:10])
-x = torch.Tensor(data[:, :8])
+threshold = 2000
 
-csnet = CSNet(8, 8)
-optim = optimizer.SGD(lr=0.1, model=csnet)
+# y_train = torch.LongTensor(data[:threshold, 10:12])
+# x_train = torch.Tensor(data[:threshold, :10])
+# y_val = torch.LongTensor(data[threshold:, 10:12])
+# x_val = torch.Tensor(data[threshold:, :10])
 
-loss_history = np.array([])
+y_train = torch.LongTensor(data[:threshold, 10:12])
+x_train = torch.Tensor(data[:threshold, :10])
+y_val = torch.LongTensor(data[threshold:, 10:12])
+x_val = torch.Tensor(data[threshold:, :10])
+
+csnet = CSNet(10, 10)
+optim = optimizer.SGD(lr=0.001, model=csnet)
+scheduler = optimizer.StepLR(optim, step_size=20000, gamma=0.1)
+loss_history_train = np.array([])
+loss_history_val = np.array([])
 x_loss = np.array([])
 
 start_time = time.time()
-for epoch in range(2000):
+for epoch in range(20000):
     optim.zero_grad()
-    pred = csnet.forward(x)
-    loss = F.CrossEntropy(y, pred)
+    pred = csnet.forward(x_train)
+    loss = F.CrossEntropy(y_train, pred)
     loss.backward()
     optim.step()
-
+    # scheduler.step()
     if epoch % 100 == 0:
         print(f'epoch: {epoch} loss: {loss}')
 
-    loss_history = np.append(loss_history, loss.detach().numpy())
+    loss_history_train = np.append(loss_history_train, loss.detach().numpy())
     x_loss = np.append(x_loss, epoch)
+
+    with torch.no_grad():
+        pred_val = csnet.forward(x_val)
+        loss_val = F.CrossEntropy(y_val, pred_val)
+        loss_history_val = np.append(loss_history_val, loss.detach().numpy())
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
-plt.plot(x_loss, loss_history)
+plt.plot(x_loss, loss_history_train, label='train')
+plt.plot(x_loss, loss_history_val, label='val')
 plt.grid(alpha=0.2)
+plt.legend()
 plt.show()
 
 with torch.no_grad():
-    pred = csnet.forward(x)
-    count = torch.count_nonzero(torch.eq(torch.argmax(pred, dim=1), torch.argmax(y, dim=1)))
+    pred = csnet.forward(x_val)
+    count = torch.count_nonzero(torch.eq(torch.argmax(pred, dim=1), torch.argmax(y_val, dim=1)))
     acc = count / pred.size(dim=0)
     print(f'Accuracy: {acc}')
-    x_pred = np.arange(pred.size(dim=0))
-
-    plt.plot(x_pred, torch.argmax(y, dim=1), label='y', alpha=0.8)
-    plt.plot(x_pred, torch.argmax(pred, dim=1), 'r', label='pred', alpha=0.8)
-    plt.legend()
-    plt.grid(alpha=0.2)
-    plt.show()
+    # x_pred = np.arange(pred.size(dim=0))
+    #
+    # plt.plot(x_pred, torch.argmax(y_train, dim=1), label='y', alpha=0.8)
+    # plt.plot(x_pred, torch.argmax(pred, dim=1), 'r', label='pred', alpha=0.8)
+    # plt.legend()
+    # plt.grid(alpha=0.2)
+    # plt.show()
